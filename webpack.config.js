58ecc33381a7b3d0002e1aa9f298d1c7eb2324c9
Terry Luan <terryluan12@gmail.com>
2023-02-04 18:51:06 -0800

    Updated webpack dependencies and allowed web assembly in webpack export

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 module.exports = {
   entry: "./bootstrap.js",
+  experiments: {
+    asyncWebAssembly: true,
+    syncWebAssembly: true,
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
