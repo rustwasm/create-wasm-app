0679aef36d7de85d2c3ca5d1f6cca00166d8eebf
Daw-Chih Liou <dawochih.liou@gmail.com>
2023-03-01 08:00:50 +0100

    feat: upgrade to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: "./bootstrap.js",
@@ -8,7 +8,16 @@ module.exports = {
     filename: "bootstrap.js",
   },
   mode: "development",
-  plugins: [
-    new CopyWebpackPlugin(['index.html'])
-  ],
+  plugins: [new CopyWebpackPlugin(["index.html"])],
+  experiments: {
+    asyncWebAssembly: true,
+    syncWebAssembly: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3000,
+  },
 };
