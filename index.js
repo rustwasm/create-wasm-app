d0b5d598c8c3259406847fb93025eefdddb6e80b
tomo813 <141240816+tomo813@users.noreply.github.com>
2024-03-09 21:55:29 +0900

    4th

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import * as wasm from "hello-wasm-pack";
+import {Universe} from "wasm-game-of-life"
 
-wasm.greet();
+wasm.greet("tom");
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+  
+    requestAnimationFrame(renderLoop);
+  };
+
+
+requestAnimationFrame(renderLoop);
